787739922db56c27cc432afa22b46d3044fd5f83
manistal <nistam328@gmail.com>
2015-03-20 22:05:20 -0400

    Added slash commandsvim buttons.md

--- CalmDownandGamble.lua
+++ CalmDownandGamble.lua
@@ -1,18 +1,28 @@
 
 
 -- Declare the new addon and load the libraries we want to use -- 
-CalmDownandGamble = LibStub("AceAddon-3.0"):NewAddon(
-    "CalmDownandGamble", "AceConsole-3.0", "AceEvent-3.0", "AceComm-3.0"
-)
+CalmDownandGamble = LibStub("AceAddon-3.0"):NewAddon("CalmDownandGamble", "AceConsole-3.0", "AceComm-3.0", "AceEvent-3.0", "AceTimer-3.0", "AceHook-3.0", "AceSerializer-3.0")
+local CalmDownandGamble	= LibStub("AceAddon-3.0"):GetAddon("CalmDownandGamble")
+
 
 -- Basic Adddon Initialization stuff, virtually inherited functions -- 
 -- ================================================================ -- 
 
+
+
+
 -- CONSTRUCTOR -- 
 function CalmDownandGamble:OnInitialize()
     -- Set up a database so we can save results -- 
+	self:Print("Load Begin")
+
+	self:RegisterChatCommand("cdgshow", "ShowUI")
+	self:RegisterChatCommand("cdgroll", "GenerateRoll")
+	self:RegisterChatCommand("cdgleave", "LeaveGame")
     self.db = LibStub("AceDB-3.0"):New("CalmDownandGambleDB")
-	self:RegisterCDCallbacks()
+	
+	self:Print("Load Complete!!")
+
 end
 
 -- INIT FOR ENABLE -- 
@@ -33,17 +43,11 @@ function CalmDownandGamble:RegisterCDCallbacks()
 
 end
 
-CalmDownandGamble:RegisterEvent("CHAT_MSG_ADDON")
-CalmDownandGamble:RegisterEvent("CHAT_MSG_CHANNEL")
-CalmDownandGamble:RegisterChatCommand("cdgroll", "GenerateRoll")
-CalmDownandGamble:RegisterChatCommand("cdgjoin", "JoinGame")
-CalmDownandGamble:RegisterChatCommand("cdgleave", "LeaveGame")
-
 -- Corresponding Chat command funcs -- 
 function CalmDownandGamble:GenerateRoll()
 end
 
-function CalmDownandGamble:JoinGame()
+function CalmDownandGamble:ShowUI(input)
 	CrossGambling_Frame:Show();
 end
 
