f340a112e0511994d6f57dbb0ee1fb04a21381ec
manistal <nistam328@gmail.com>
2015-03-21 10:57:38 -0400

    cleaned up buttons

--- CalmDownandGamble.lua
+++ CalmDownandGamble.lua
@@ -1,16 +1,16 @@
 
 
--- Declare the new addon and load the libraries we want to use -- 
+-- Declare the new addon and load the libraries we want to use 
 CalmDownandGamble = LibStub("AceAddon-3.0"):NewAddon("CalmDownandGamble", "AceConsole-3.0", "AceComm-3.0", "AceEvent-3.0", "AceTimer-3.0", "AceHook-3.0", "AceSerializer-3.0")
 local CalmDownandGamble	= LibStub("AceAddon-3.0"):GetAddon("CalmDownandGamble")
 local AceGUI = LibStub("AceGUI-3.0")
 
--- Basic Adddon Initialization stuff, virtually inherited functions -- 
--- ================================================================ -- 
+-- Basic Adddon Initialization stuff, virtually inherited functions 
+-- ================================================================ 
 
--- CONSTRUCTOR -- 
+-- CONSTRUCTOR 
 function CalmDownandGamble:OnInitialize()
-    -- Set up a database so we can save results -- 
+    -- Set up a database so we can save results 
 	self:Print("Load Begin")
 
 	self:RegisterChatCommand("cdgwoo", "ShowUI")
@@ -20,21 +20,19 @@ function CalmDownandGamble:OnInitialize()
 	self:ConstructUI()
 
 	self:Print("Load Complete!!")
-
 end
 
--- INIT FOR ENABLE -- 
+-- INIT FOR ENABLE  
 function CalmDownandGamble:OnEnable()
-    
-	
-
 end
 
--- DESTRUCTOR -- 
+-- DESTRUCTOR  
 function CalmDownandGamble:OnDisable()
 end
 
--- Corresponding Chat command funcs -- 
+
+-- CALLBACK FUNCTIONS 
+-- ==================================================== 
 function CalmDownandGamble:GenerateRoll()
 end
 
@@ -50,37 +48,70 @@ function CalmDownandGamble:ButtonCallback()
 	self:Print("DID IT WORK")
 end
 
--- UI ELEMENTS --
--- ============================== -- 
+-- UI ELEMENTS 
+-- ======================================================
 function CalmDownandGamble:ConstructUI()
 	
 	-- Settings to be used -- 
 	local cdg_ui_elements = {
-		
+		-- Main Box Frame -- 
 		main_frame = {
 			width = 450,
 			height = 150
 		},
 		
+		-- Order in which the buttons are layed out -- 
+		button_index = {
+			"accept_entries",
+			"start_gambling",
+			"print_stats_table",
+			"print_ban_list",
+			"roll_for_me",
+			"enter_for_me",
+			"chat_channel",
+			"reset_game"
+		},
+		
+		-- Button Definitions -- 
 		buttons = {
-			accept_entries = {
-				width = 125,
-				label = "Accept Entries",
+			chat_channel = {
+				width = 100,
+				label = "(Raid)",
+				click_callback = function() self:ButtonCallback() end
+			},
+			reset_game = {
+				width = 100,
+				label = "Reset",
+				click_callback = function() self:ButtonCallback() end
+			},
+			roll_for_me = {
+				width = 100,
+				label = "Roll For Me",
+				click_callback = function() self:ButtonCallback() end
+			},
+			enter_for_me = {
+				width = 100,
+				label = "Enter Me",
+				click_callback = function() self:ButtonCallback() end
+			},
+			start_gambling = {
+				width = 100,
+				label = "StartRolls!",
 				click_callback = function() self:ButtonCallback() end
 			},
 			print_ban_list = {
-				width = 125,
+				width = 100,
 				label = "Print Bans",
 				click_callback = function() self:ButtonCallback() end
 			},
 			print_stats_table = {
-				width = 125,
+				width = 100,
 				label = "Print Stats",
 				click_callback = function() self:ButtonCallback() end
 			},
-			roll_for_me = {
-				width = 125,
-				label = "Roll For Me",
+			accept_entries = {
+				width = 100,
+				label = "CallEntries",
 				click_callback = function() self:ButtonCallback() end
 			}
 		}
@@ -99,7 +130,9 @@ function CalmDownandGamble:ConstructUI()
 	
 
 	-- Set up Buttons -- 
-	for button_name, button_settings in pairs(cdg_ui_elements.buttons) do
+	for _, button_name in pairs(cdg_ui_elements.button_index) do
+		local button_settings = cdg_ui_elements.buttons[button_name]
+	
 		self.ui[button_name] = AceGUI:Create("Button")
 		self.ui[button_name]:SetText(button_settings.label)
 		self.ui[button_name]:SetWidth(button_settings.width)
