7ba3e58b89c221e8c5042a7cc26241eb733c7a7a
manistal <nistam328@gmail.com>
2015-03-20 20:36:56 -0400

    added xml and some embeds

--- CalmDownandGamble.lua
+++ CalmDownandGamble.lua
@@ -12,11 +12,14 @@ CalmDownandGamble = LibStub("AceAddon-3.0"):NewAddon(
 function CalmDownandGamble:OnInitialize()
     -- Set up a database so we can save results -- 
     self.db = LibStub("AceDB-3.0"):New("CalmDownandGambleDB")
+	self:RegisterCDCallbacks()
 end
 
 -- INIT FOR ENABLE -- 
 function CalmDownandGamble:OnEnable()
-    self:RegisterCDCallbacks()
+    
+	
+
 end
 
 -- DESTRUCTOR -- 
@@ -27,19 +30,21 @@ end
 -- Some Util functions to clean up life -- 
 -- =================================== -- 
 function CalmDownandGamble:RegisterCDCallbacks()
-    self:RegisterEvent("CHAT_MSG_ADDON")
-    self:RegisterEvent("CHAT_MSG_CHANNEL")
-	self:RegisterChatCommand("cdgroll", "GenerateRoll")
-	self:RegisterChatCommand("cdgjoin", "JoinGame")
-	self:RegisterChatCommand("cdgleave", "LeaveGame")
+
 end
 
+CalmDownandGamble:RegisterEvent("CHAT_MSG_ADDON")
+CalmDownandGamble:RegisterEvent("CHAT_MSG_CHANNEL")
+CalmDownandGamble:RegisterChatCommand("cdgroll", "GenerateRoll")
+CalmDownandGamble:RegisterChatCommand("cdgjoin", "JoinGame")
+CalmDownandGamble:RegisterChatCommand("cdgleave", "LeaveGame")
 
 -- Corresponding Chat command funcs -- 
 function CalmDownandGamble:GenerateRoll()
 end
 
 function CalmDownandGamble:JoinGame()
+	CrossGambling_Frame:Show();
 end
 
 function CalmDownandGamble:LeaveGame()
