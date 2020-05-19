cb9b91c42ce66393241bda0d22206f7677238d0b
Miguel Nistal <nistam328@gmail.com>
2020-05-19 18:08:49 -0400

    Cleaned up the deploy

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,20 @@
 
 TAGNAME=$1
 
+# Tag up
 git tag $TAGNAME 2> /dev/null && git push origin $TAGNAME
 
+# Set up
 SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 RELEASE_DIR="/tmp/CDGRelease/CalmDownandGamble"
 mkdir -p /tmp/CDGRelease
 
+# Copy up 
 cp -rf "${SCRIPT_PATH}" "${RELEASE_DIR}"
 rm -rf "${RELEASE_DIR}/.git"
 
+# Zip up 
 zip -r CalmDownandGamble-${TAGNAME}.zip ${RELEASE_DIR}
+
+# Cleanup
+rm -rf /tmp/CDGRelease
