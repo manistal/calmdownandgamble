67f54a8a14f93bb7f7ef72cc89cc17416962596b
Miguel Nistal <nistam328@gmail.com>
2022-02-13 11:23:51 -0800

    Updated deploy

--- deploy.sh
+++ deploy.sh
@@ -7,18 +7,20 @@ git tag $TAGNAME 2> /dev/null && git push origin $TAGNAME
 
 # Set up
 SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-RELEASE_DIR="/tmp/CDGRelease/CalmDownandGamble"
+RELEASE_DIR="/tmp/CDGRelease/"
 mkdir -p /tmp/CDGRelease
 
 # Copy up 
 cp -rf "${SCRIPT_PATH}" "${RELEASE_DIR}"
-rm -rf "${RELEASE_DIR}/.git"
-rm -rf "${RELEASE_DIR}/.gitignore"
-rm -rf "${RELEASE_DIR}/deploy.sh"
+rm -rf "${RELEASE_DIR}/CalmDownandGamble/.git"
+rm -rf "${RELEASE_DIR}/CalmDownandGamble/.gitignore"
+rm -rf "${RELEASE_DIR}/CalmDownandGamble/deploy.sh"
 
 # Zip up 
 cd ${RELEASE_DIR}
-zip -r CalmDownandGamble-${TAGNAME}.zip "CalmDownandGamble"
+zip -r CalmDownandGamble-${TAGNAME}.zip CalmDownandGamble
 
 # Cleanup
+cd -
+mv ${RELEASE_DIR}/*zip .
 rm -rf /tmp/CDGRelease
